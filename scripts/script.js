6bd8261e49daa8cb2212f819e19036fa20380839
Neda <nedachangizi@gmail.com>
2023-03-20 10:42:50 -0700

    update para

--- scripts/script.js
+++ scripts/script.js
@@ -22,10 +22,23 @@
 // greet();
 
 const enteredValue = prompt("Enter a value");
+
+
+const radiusPara = document.querySelector("#radius");
+radiusPara.textContent +=  enteredValue;
+// console.log(radiusPara);
+const resultPara = document.querySelector("#result");
+// console.log(resultPara);
+
 function calculateArea(radius) {
+  // references to p tags
+
   // check if radius is not a number
   if (isNaN(radius)) {
-    alert("This is not a number");
+    // alert("This is not a number");
+    // update the text of resultPara to show the error
+    resultPara.textContent = "This is not a number"
+    
   } else {
     const area = Math.PI * radius * radius;
     return area.toFixed(2);
@@ -34,5 +47,8 @@ function calculateArea(radius) {
 
 let result = calculateArea(enteredValue);
 if (result) {
-  alert(`The area of a circle with radius ${enteredValue} is ${result}`);
+  // update the radiusPara
+  // alert(`The area of a circle with radius ${enteredValue} is ${result}`);
+  resultPara.textContent =`The area of a circle with radius ${enteredValue} is ${result}`
 }
+
