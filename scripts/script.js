929a1662368eef0b31ffb184f742008a7eaa67b9
Neda <nedachangizi@gmail.com>
2023-03-20 10:08:03 -0700

    area of circle function

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 // function declaration
-function greet(name, time="day")
-{
-    console.log(`Good ${time} ${name}`);
-}
-// function calling
-greet("Neda");
+// function greet(name, time="day")
+// {
+//     console.log(`Good ${time} ${name}`);
+// }
+// // function calling
+// greet("Neda","night");
 
 // // function expression
 // const greet = function (name)
@@ -20,3 +20,19 @@ greet("Neda");
 // }
 // // function calling
 // greet();
+
+const enteredValue = prompt("Enter a value");
+function calculateArea(radius) {
+  // check if radius is not a number
+  if (isNaN(radius)) {
+    alert("This is not a number");
+  } else {
+    const area = Math.PI * radius * radius;
+    return area.toFixed(2);
+  }
+}
+
+let result = calculateArea(enteredValue);
+if (result) {
+  alert(`The area of a circle with radius ${enteredValue} is ${result}`);
+}
